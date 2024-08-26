fb8b6483a1b8b0a592a56d82f584761cefc7276e
HamzaAziz0320 <118163884+HamzaAziz0320@users.noreply.github.com>
2024-08-26 18:02:33 +0500

    Build Cache Data Testing

--- Build/fullscreen.js
+++ Build/fullscreen.js
@@ -1,4 +1,5 @@
-function toggleFullScreen() {
+function toggleFullScreen() 
+{
     if (!document.fullscreenElement) {
         document.documentElement.requestFullscreen().catch(err => {
             console.log(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
@@ -7,3 +8,44 @@ function toggleFullScreen() {
         document.exitFullscreen();
     }
 }
+
+//function getLocalStorageData() {
+//    return {
+//        id: localStorage.getItem("id"),
+//        token: localStorage.getItem("token")
+//    };
+//}
+
+function getLocalStorageData() {
+    // Fetch the data from localStorage
+    var id = localStorage.getItem("id");
+    var token = localStorage.getItem("token");
+
+    // Create a JSON object to store the data
+    var data = {
+        id: id,
+        token: token
+    };
+
+    // Convert the JSON object to a string and return it
+    return JSON.stringify(data);
+}
+
+function toggleFullScreenOnIOS() 
+{
+    var doc = window.document;
+    var docEl = doc.documentElement;
+
+    var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+    var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+    if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+        requestFullScreen.call(docEl);
+    } else {
+        cancelFullScreen.call(doc);
+    }
+}
+
+document.getElementById('yourButtonId').addEventListener('click', function() {
+    toggleFullScreenOnIOS();
+});
